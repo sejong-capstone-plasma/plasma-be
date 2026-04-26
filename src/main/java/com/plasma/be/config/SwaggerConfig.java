74ae34f8957df62e698bdf95233e499921258e3e
Jihwan Yeom <yum020428@gmail.com>
2026-04-26 11:38:37 +0900

    chore : swagger config 변경

--- src/main/java/com/plasma/be/config/SwaggerConfig.java
+++ src/main/java/com/plasma/be/config/SwaggerConfig.java
@@ -2,16 +2,12 @@ package com.plasma.be.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    // Swagger/OpenAPI 문서의 기본 메타데이터를 설정
     @Bean
     public OpenAPI plasmaOpenAPI() {
         return new OpenAPI()
@@ -22,9 +18,6 @@ public class SwaggerConfig {
 
                                 자연어 기반 공정 분석 요청을 접수하고, AI 서버를 통해 공정 파라미터(pressure, source_power, bias_power)를 추출·검증하여 반환합니다.
                                 """)
-                        .version("v1.0"))
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("로컬 개발 서버")
-                ));
+                        .version("v1.0"));
     }
 }
